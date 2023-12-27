b650595a127d35f39e9ddce30401a8e96c8a8021
antemortem <agos.dev22@gmail.com>
2023-12-27 14:35:23 -0300

    Arreglo validacion para agregar reserva

--- RestaurantApi/Repository/ReservaRepository.cs
+++ RestaurantApi/Repository/ReservaRepository.cs
@@ -17,45 +17,57 @@ namespace RestaurantApi.Repository
 
         public async Task<bool> AddNewReservaAsync(ReservaDTO reserva)
         {
-           
-            var newReserva = new Reserva();
-
-            //Generacion de codigo unico para la reserva
+            // Generación de código único para la reserva
             Guid id = Guid.NewGuid();
 
-            newReserva.CodReserva = id.ToString();
-            newReserva.NombrePersona = reserva.NombrePersona; 
-            newReserva.ApellidoPersona = reserva.ApellidoPersona;
-            newReserva.Celular = reserva.Celular;
-            newReserva.Mail = reserva.Mail;
-            newReserva.Dni = reserva.Dni;
-            newReserva.IdRangoReserva = reserva.IdRangoReserva;
-            newReserva.CantidadPersonas = reserva.CantidadPersonas;
-            newReserva.FechaReserva = reserva.FechaReserva;
-            newReserva.FechaAlta = DateTime.Now;
-            newReserva.FechaModificacion = DateTime.Now;
-
-            //Restar a los cupos por turno
-            var rangoReserva = await _restaurantContext.RangoReservas.FindAsync(reserva.IdRangoReserva);
-
-            /*if (rangoReserva != null)
+            var newReserva = new Reserva
             {
-                // Restar la cantidad de personas del cupo
-                rangoReserva.Cupo -= reserva.CantidadPersonas;
+                CodReserva = id.ToString(),
+                NombrePersona = reserva.NombrePersona,
+                ApellidoPersona = reserva.ApellidoPersona,
+                Celular = reserva.Celular,
+                Mail = reserva.Mail,
+                Dni = reserva.Dni,
+                IdRangoReserva = reserva.IdRangoReserva,
+                CantidadPersonas = reserva.CantidadPersonas,
+                FechaReserva = reserva.FechaReserva,
+                FechaAlta = DateTime.Now,
+                FechaModificacion = DateTime.Now
+            };
 
-                // Guardar los cambios en la base de datos
-                await _restaurantContext.SaveChangesAsync();
-            }*/
+            // Validar si hay cupo disponible para la fecha y el rango de reserva
+            var rangoReserva = await _restaurantContext.RangoReservas
+                .Where(rr => rr.IdRangoReserva == reserva.IdRangoReserva)
+                .FirstOrDefaultAsync();
 
+            if (rangoReserva != null)
+            {
+                var reservasEnFecha = await _restaurantContext.Reservas
+                    .Where(r => r.IdRangoReserva == reserva.IdRangoReserva && r.FechaReserva.Date == reserva.FechaReserva.Date)
+                    .SumAsync(r => r.CantidadPersonas);
 
-            reserva.Estado.ToUpper();
+                int cupoDisponible = rangoReserva.Cupo - reservasEnFecha - reserva.CantidadPersonas;
 
+                if (cupoDisponible < 0)
+                {
+                    // No hay suficiente cupo disponible para la reserva en esa fecha y rango
+                    return false;
+                }
+            }
+            else
+            {
+                // El ID de rango de reserva proporcionado no es válido
+                return false;
+            }
+
+            
             await _restaurantContext.Reservas.AddAsync(newReserva);
 
             int rows = await _restaurantContext.SaveChangesAsync();
 
             return rows > 0;
         }
+
 
     }
 }
